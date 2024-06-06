bb4b137a3e8202d59eebc58480f252105585ef40
Exousia KASEKA <kasekaexousia@gmail.com>
2024-06-06 11:56:20 +0100

    liaison des fichier static

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 3000;
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
   res.render("index");
